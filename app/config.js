9662b766dc4ebcd4dbe1658879bf2098e575fe60
Bankhead22 <djbankhead0@gmail.com>
2022-04-18 12:04:34 -0500

    fixed config url

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-	production: 'https://murmuring-dusk-80041.herokuapp.com/',
+	production: 'https://murmuring-dusk-80041.herokuapp.com',
 	development: 'http://localhost:4741',
 }
 
